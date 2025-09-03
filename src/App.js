3427c27c6e757d7151e2d99e849160f396511578
Davut Simsek <davutt.simsek@gmail.com>
2025-09-03 22:59:51 +0200

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React META Front-End Developer capstone project
         </a>
       </header>
     </div>
